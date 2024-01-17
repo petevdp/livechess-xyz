e80ce8a3db4b33d5ce39fbfade6e58592514585f
petevdp <pjvanderpol@gmail.com>
2024-01-17 13:15:57 -0800

    add app manifest and icons

---
+++ gen_icons.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# generates all icons needed in app manifest
+
+LOGO="./src/assets/icons/logo.svg"
+
+inkscape $LOGO -o public/icons/192.png -w 192 -h 192
+inkscape $LOGO -o public/icons/384.png -w 384 -h 384
+inkscape $LOGO -o public/icons/512.png -w 512 -h 512
+inkscape $LOGO -o public/icons/1024.png -w 1024 -h 1024
