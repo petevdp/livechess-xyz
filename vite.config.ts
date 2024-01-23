3666c7851819a429daff0a3028c73e097bab755c
petevdp <pjvanderpol@gmail.com>
2024-01-22 22:15:12 -0800

    migrate off of vite-plugin-favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,11 @@
-import devtools from 'solid-devtools/vite';
-import { defineConfig } from 'vite';
-import solid from 'vite-plugin-solid';
-import solidSvg from 'vite-plugin-solid-svg';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { ViteFaviconsPlugin } from 'vite-plugin-favicon';
-
+import devtools from 'solid-devtools/vite'
+import { defineConfig } from 'vite'
+import solid from 'vite-plugin-solid'
+import solidSvg from 'vite-plugin-solid-svg'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-	plugins: [devtools({ autoname: true }), solid(), solidSvg(), tsconfigPaths(), ViteFaviconsPlugin({logo: 'favicon.svg', favicons: {
-			appName: 'livechess.xyz',
-			appDescription: 'Play chess with friends online',
-			developerName: 'Pieter Vanderpol',
-			developerURL: 'https://github.com/petevdp',
-			background: '#ddd',
-			theme_color: 'hsl(221.2 83.2% 53.3%)',
-			icons: {
-				coast: false,
-				yandex: false
-			}
-		}})],
+	plugins: [devtools({ autoname: true }), solid(), solidSvg(), tsconfigPaths()],
 	server: {
 		proxy: {
 			'/networks': { target: 'http://0.0.0.0:8080', changeOrigin: true },
