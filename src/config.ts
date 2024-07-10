38cf5817a4a089ac056938b0141aec74041ef0f8
petevdp <pjvanderpol@gmail.com>
2024-07-10 11:38:54 -0700

    make config.ts cross platform

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,20 @@
-export const ENVIRONMENT = import.meta.env.ENVIRONMENT || 'development'
+export const ENVIRONMENTS = ['development', 'production'] as const
+export let ENVIRONMENT: typeof ENVIRONMENTS[number] = 'development'
+let loc: URL | Location
+if (typeof window === 'undefined') {
+	const dotenv = await import('dotenv')
+	dotenv.config()
+	// probably won't be used, but we'll set it up correctly anyway :shrug:
+	loc = new URL(`http://${process.env.HOST}:${process.env.PORT}`)
+	ENVIRONMENT = process.env.NODE_ENV as typeof ENVIRONMENTS[number] ?? ENVIRONMENT
+} else {
+	ENVIRONMENT = import.meta.env.ENVIRONMENT ?? ENVIRONMENT
+	loc = window.location
+}
+
 export const PLAYER_TIMEOUT = 5000
-export const API_URL = `${window.location.protocol}//${window.location.host}/api`;
-export const WS_API_URL = `${window.location.protocol === 'https:' ? 'wss' : 'ws'}://${window.location.host}/api`
+export const API_URL = `${loc.protocol}//${loc.host}/api`;
+export const WS_API_URL = `${loc.protocol === 'https:' ? 'wss:' : 'ws:'}//${loc.host}/api`
 
 export const BOARD_COLORS = {
 	light: '#eaaa69',
